2a99241cf02a27e1a110bcfbd375df26227c698f
jihengyang <jihengyang@bytedance.com>
2023-01-09 21:51:57 +0800

    表5增加注释

--- 113071-V1/Table5.m
+++ 113071-V1/Table5.m
@@ -3,8 +3,8 @@ initial_eqm
 
 % Migration Weights in 2000
 weight=(L0*ones(1,2*(N-1))).*mij2000.*(ones(2*(N-1),2*(N-1))-eye(2*(N-1),2*(N-1)));
-% weight=weight.*repmat([kron(ones(N-1,1),[0;1]) kron(ones(N-1,1),[1;0])],1,N-1); % turn on for between ag and non-ag
-% weight=weight.*(1-kron(eye(N-1,N-1),ones(2,2))); % turn on for just between-province migration
+% weight=weight.*repmat([kron(ones(N-1,1),[0;1]) kron(ones(N-1,1),[1;0])],1,N-1); % turn on for between ag and non-ag，等于表格里的overall
+% weight=weight.*(1-kron(eye(N-1,N-1),ones(2,2))); % turn on for just between-province migration，等于省际移民成本变化-整体
 % weight=weight.*kron(eye(N-1,N-1),ones(2,2)); % turn on for just within-province migration
 % weight=weight.*repmat([kron(ones(N-1,1),[1;0]) kron(ones(N-1,1),[0;0])],1,N-1); % turn on for within ag
 % weight=weight.*repmat([kron(ones(N-1,1),[0;0]) kron(ones(N-1,1),[0;1])],1,N-1); % turn on for within non-ag
